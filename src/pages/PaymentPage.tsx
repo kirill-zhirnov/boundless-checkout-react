f9766a7c777575dff791a4e6745bb1c69bf53dda
keXXpert <kexxpert@ya.ru>
2022-03-01 19:11:54 +0300

    added loaders on pages navigation

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -10,21 +10,30 @@ import Loading from '../components/Loading';
 export default function PaymentPage() {
 	const {isInited} = useInitCheckoutByCart();
 	const [paymentPage, setPaymentPage] = useState<null|ICheckoutPaymentPageData>(null);
+	const [loading, setLoading] = useState(false);
 	const {api, order} = useAppSelector(state => state.app);
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
 		if (api && order) {
+			setLoading(true);
 			const promise = api.checkout.getPaymentPage(order.id)
 				.then((data) => setPaymentPage(data))
+				.finally(() => setLoading(false))
 			;
 			dispatch(addPromise(promise));
 		}
 	}, [api, order]);//eslint-disable-line
 
-	if (!isInited || !paymentPage) {
+	if (!isInited) {
 		return <Loading />;
 	}
+
+	if (loading || !paymentPage) return (
+		<CheckoutLayout>
+			<Loading />
+		</CheckoutLayout>
+	);
 
 	return (
 		<CheckoutLayout>
